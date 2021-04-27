eeb1942b39bce2099c9e41f95b7e393fd2537dbc
Natchapol Srisang <utopiabeam@gmail.com>
2021-04-28 01:03:44 +0700

    feat(action): add special action data

---
+++ data/SpecialActionData.ts
@@ -0,0 +1,23 @@
+export enum SpecialAction {
+  Investigate = 'Investigate',
+  Expose = 'Expose',
+  Spy = 'Spy',
+}
+
+export interface SpecialActionDetail {
+  type: number;
+  name: SpecialAction;
+  cost: number;
+}
+
+export class SpecialActionData {
+  public static specialActions: SpecialActionDetail[] = [
+    { type: 0, name: SpecialAction.Investigate, cost: 75 },
+    { type: 1, name: SpecialAction.Expose, cost: 125 },
+    { type: 2, name: SpecialAction.Spy, cost: 50 },
+  ];
+
+  public static getSpecialAction(type: number) {
+    return this.getSpecialAction[type];
+  }
+}
