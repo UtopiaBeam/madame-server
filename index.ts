3153acefb191934adc7bfc1ea77421683388a0f1
Natchapol Srisang <utopiabeam@gmail.com>
2021-04-28 02:42:33 +0700

    fix(socket): fix socket never execute

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import * as http from 'http';
 import { Server } from 'socket.io';
 import router from './routes';
 import * as cors from 'cors';
+import { GameStore } from './stores/GameStore';
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,15 @@ export const io = new Server(server, {
     origin: '*',
     methods: ['GET', 'POST'],
   },
+});
+
+io.on('connection', client => {
+  client.on('join-game', gameId => {
+    client.join(gameId);
+    const game = GameStore.findOne(gameId);
+    const players = game.players.map(p => p.info);
+    io.to(gameId).emit('new-player', players);
+  });
 });
 
 server.listen(3000, () => console.log('🚀 Listening on http://localhost:3000'));
