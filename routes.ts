9ee027476db7bda9e4d14630342e914de1f2d52d
Natchapol Srisang <utopiabeam@gmail.com>
2021-06-09 22:42:36 +0700

    fix(game): fix not response

--- routes.ts
+++ routes.ts
@@ -150,6 +150,7 @@ router.post(
       const game = GameStore.findOne(req.body.gameId);
       const player = game.findPlayer(req.body.playerId);
       player.unplaceCardFromChannel(req.body.channelType);
+      res.sendStatus(201);
     } catch (error) {
       res.status(400).send({ error: error.message });
     }
